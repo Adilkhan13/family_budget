2444eebd0475bfe6f8d01f1e17fb44653a78ed06
Зайдулла Адилхан <AZaidulla@mortgage.local>
2023-09-29 18:59:15 +0600

    add spend line plot

--- streamlit_graphs.py
+++ streamlit_graphs.py
@@ -64,12 +64,37 @@ def line_catspends_by_months(data,selected_options,categories,**kwargs)->None:
     
     data = data[['month','price']].groupby('month').sum()['price']
     data = data.reset_index()
-    fig = px.line(data, x = 'month', y = 'price', markers=True, title= 'Затраты по категории по месяцам')
+    data['price_thousands'] = (data['price']/1000).round().astype(str).str[:-2] + " тыс."
+    fig = px.bar(data, x = 'month', y = 'price', title= 'Затраты по категории по месяцам, тыс. тг', text = 'price_thousands')
 
     st.write(fig)
 
+
+def line_spends(data:pd.DataFrame ,start_date,end_date,selected_options,**kwargs)->None:
+
+    mask = (
+        (data['date'].dt.date >= start_date)&
+        (data['date'].dt.date <= end_date)&
+        (data['name'].isin(selected_options))&
+        (~data['category'].isin(['С KASPI ДЕПОЗИТА','НА KASPI ДЕПОЗИТ']))
+    )
+    data = data[mask]
+    incomes = data[data['category']=='ЗАРПЛАТА']['price'].sum()
+    
+    data['spends'] = (data[data['price']<0]['price'] * -1).cumsum()
+    data['spends'] = data['spends'].fillna(method="backfill")
+    st.dataframe(data)
+    data['spends_thousands'] = (data['spends']/1000).round().astype(str).str[:-2] + " тыс."
+    fig = px.line(data, x = 'date', y = 'spends', title= 'Затраты', markers='-*')
+    fig.add_hrect(y0=incomes,y1=data['spends'].max(),  line_width=0, fillcolor="red", opacity=0.2)
+    fig.add_vline(x = data[data['spends'] > incomes].iloc[0].date, line_width=3, line_dash="dash", line_color="green")
+
+    st.write(fig)
+
+
 GRAPH_DICT = {
-        'Затраты по категориям':hbar_spends, 
-        'Соотношение доходов и расходов':bar_income_spend_compare, 
-        'Затраты по категории по месяцам':line_catspends_by_months,
+    "Траты за месяц":line_spends,
+    'Затраты по категориям':hbar_spends, 
+    'Соотношение доходов и расходов':bar_income_spend_compare, 
+    'Затраты по категории по месяцам':line_catspends_by_months,   
     }
